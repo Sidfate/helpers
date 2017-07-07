94a4012210e4ce18f5a1396faa04c5f14ec5ce15
Sidfate <sidfate@163.com>
2017-07-07 16:10:15 +0800

    add VerifyTest

--- src/Helpers/Verify.php
+++ src/Helpers/Verify.php
@@ -28,9 +28,9 @@ class Verify
 	 */
 	public static function isPhone($phone)
 	{
-		if(empty($ip)) return false;
-		
-    	return preg_match('#^13[\d]{9}$|^14[5,7]{1}\d{8}$|^15[^4]{1}\d{8}$|^17[0,6,7,8]{1}\d{8}$|^18[\d]{9}$#', $phone) ? true : false;
+		if(empty($phone)) return false;
+
+    	return preg_match('/^13[\d]{9}$|^14[5,7]{1}\d{8}$|^15[^4]{1}\d{8}$|^17[0,6,7,8]{1}\d{8}$|^18[\d]{9}$/', $phone) ? true : false;
 	}
 
 	/**
